7514bab29f9f47217da6f3ffea4b23730106bcf1
Colearo <colearolu@icloud.com>
2018-05-07 22:41:20 +0800

    scheduler.py:refine the code
    Signed-off-by: Colearo <colearolu@icloud.com>

--- scheduler.py
+++ scheduler.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import time
+import datetime
 from thoth_eyes.sched import ThothEyes
 from thoth_eyes.orm import ORM
 
@@ -10,6 +11,10 @@ thoth_eyes = ThothEyes()
 thoth_eyes.initialize()
 thoth_eyes.refresh_today_news()
 thoth_eyes.stop()
+
+today = datetime.datetime.today()
+today = today.strftime('%Y/%m/%d %H:%M')
+print('[%s]' % today)
 
 duration = time.time() - start
 print('Runs %.2f s' % duration)
